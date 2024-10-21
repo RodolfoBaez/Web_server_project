53205d7864e41e8c15358ff4d6a022a047e86536
RodolfoBaez <47578524+RodolfoBaez@users.noreply.github.com>
2024-10-21 00:59:02 -0400

    midterm Boilerplate

---
+++ midterm/Client/src/models/dataEnvelope.ts
@@ -0,0 +1,12 @@
+/* B"H */
+
+export interface DataEnvelope<T> {
+  data: T
+  error?: string
+}
+
+export interface DataListEnvelope<T> extends DataEnvelope<T[]> {
+  data: T[]
+  total: number
+  error?: string
+}
